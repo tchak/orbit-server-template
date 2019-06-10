a0067fcc257f18367cb49442758728c85a774483
Paul Chavard <git@paul.chavard.net>
2019-06-10 12:27:21 +0200

    Add inflected

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const { pluralize, singularize } = require('inflected');
 const { Schema } = require('@orbit/data');
 const { Plugin, SQLSource } = require('@orbit-server/fastify');
 
 module.exports = function(fastify, opts, next) {
   const schemaJson = require('./schema.json');
+  schemaJson.pluralize = pluralize;
+  schemaJson.singularize = singularize;
   const schema = new Schema(schemaJson);
   const source = new SQLSource({
     schema,
