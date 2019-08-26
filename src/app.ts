336df6e8fab0181315829550f7a42375aa8575b8
Paul Chavard <git@paul.chavard.net>
2019-08-27 00:01:14 +0200

    Add test runner

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,3 @@
-import { pluralize, singularize } from 'inflected';
-import { Schema, ModelDefinition } from '@orbit/data';
-import Server from '@orbit-server/fastify';
-import SQLSource from 'orbit-sql';
-import plugin from 'fastify-plugin';
+import App from './';
 
-import schemaJson from './schema.json';
-
-module.exports = plugin(function(fastify, _, next) {
-  const schema = new Schema({
-    models: schemaJson.models as Record<string, ModelDefinition>,
-    pluralize,
-    singularize
-  });
-  const source = new SQLSource({
-    schema,
-    knex: {
-      client: 'sqlite3',
-      connection: { filename: ':memory:' },
-      useNullAsDefault: true
-    }
-  });
-
-  const server = new Server({
-    source,
-    jsonapi: true,
-    graphql: true
-  });
-
-  fastify.register(server.createHandler());
-
-  next();
-});
+module.exports = App;
---
+++ test/acceptance/planet-test.ts
@@ -0,0 +1,61 @@
+import setupTest from '../helpers/setup-test';
+import App from '../../src';
+
+QUnit.module('planet', function(hooks) {
+  const context = setupTest(App, hooks);
+
+  QUnit.module('empty', function() {
+    QUnit.test('findRecords', async function(assert) {
+      let response = await context.request('/planets');
+
+      assert.deepEqual(response.body, { data: [] });
+    });
+
+    QUnit.test('findRecord', async function(assert) {
+      let response = await context.request('/planets/earth');
+
+      assert.equal(response.status, 404);
+    });
+  });
+
+  QUnit.module('with record', function(hooks) {
+    hooks.beforeEach(async function() {
+      await context.request('/planets', {
+        method: 'POST',
+        payload: {
+          data: {
+            type: 'planets',
+            id: 'earth',
+            attributes: {
+              name: 'Earth'
+            }
+          }
+        }
+      });
+    });
+
+    QUnit.test('findRecords', async function(assert) {
+      let response = await context.request('/planets');
+
+      assert.deepEqual(response.body, { data: [{
+        type: 'planets',
+        id: 'earth',
+        attributes: {
+          name: 'Earth'
+        }
+      }] });
+    });
+
+    QUnit.test('findRecord', async function(assert) {
+      let response = await context.request('/planets/earth');
+
+      assert.deepEqual(response.body, { data: {
+        type: 'planets',
+        id: 'earth',
+        attributes: {
+          name: 'Earth'
+        }
+      } });
+    });
+  });
+});
